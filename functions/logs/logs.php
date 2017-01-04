9a6608e72230ff0fb2ad17701aace102e7ae27e5
bacaAlexandre <Alexandre76b@aol.com>
2017-01-04 14:01:22 +0100

    mise à jour logs.php

--- functions/logs/logs.php
+++ functions/logs/logs.php
@@ -1,16 +1,23 @@
 <?php
 
-
+//crréation de la classe
 class logs {
 
+  public $day;
+  public $time;
+  public $string;
+  
   public static function erreur($erreur) {
-    $day = date ("d/m/y");
-    $time = "[".date ("H:i:s")."]";
-    $string = $day.$time.$erreur."\n";
-
-    $handle = fopen ("./functions/logs/$day.error.text", "a");
+    $day = date ("d.m.y");// varaible pour le jour/mois/année
+    $time = "[".date ("H:i:s")."]";//variable pour l'heure
+    $string = $time."\n".$erreur."\n\n";// variable de l'heure + erreur
+    //creation si il n'existe pas et ouverture du fichier
+    $handle = fopen ("./functions/logs/erreur/".$day.".txt", "a");
+    //ecriture de l'erreur dans le fichier ouvert
     fwrite($handle, $string);
+    //fermeture du fichier
     fclose($handle);
+
   }
 
 }
